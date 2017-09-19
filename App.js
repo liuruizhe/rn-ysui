0e90fa6d8965497be226bb7e914f9298fbe887a7
shuqiong2015 <543400696@qq.com>
2017-09-19 15:43:40 +0800

    Add files via upload
    example

---
+++ App.js
@@ -0,0 +1,142 @@
+import React from 'react';
+import { StyleSheet, Text, View, TouchableHighlight,Button} from 'react-native';
+import { StackNavigator } from 'react-navigation';
+import ToastExample from './example/Toast';
+import ButtonExample from './example/Button';
+import InputExample from './example/Input';
+import ListExample from './example/List';
+import DialogExample from './example/Dialog';
+
+class HomeScreen extends React.Component {
+  static navigationOptions = {
+    title: 'React native组件调用举例',
+  }
+  render(){
+    const { navigate } = this.props.navigation;
+    return (
+      <View style={styles.container}>
+        <View>
+          <Text style={styles.title}>表单</Text>
+          <Button
+          onPress={()=>navigate('ButtonExample')}
+          title="Button      >"
+        />
+        <Button
+          style={styles.ysuiBtn}
+          onPress={()=>navigate('ListExample')}
+          title="List      >"
+        />
+        <Button
+          style={styles.ysuiBtn}
+          onPress={()=>navigate('InputExample')}
+          title="Input      >"
+        />
+        </View>
+         <View>
+          <Text style={styles.title}>操作反馈</Text>
+          <Button
+          style={styles.ysuiBtn}
+          onPress={()=>navigate('DialogExample')}
+          title="Dialog      >"
+        />
+        <Button
+          style={styles.ysuiBtn}
+          onPress={()=>navigate('ToastExample')}
+          title="Toast      >"
+        />
+        </View>
+       </View>
+      );
+  }
+}
+
+class ButtonExampleScreen extends React.Component {
+  static navigationOptions = {
+    title: 'Button 按钮',
+  }
+  render(){
+    return (
+       <ButtonExample />
+      );
+  }
+}
+class ListExampleScreen extends React.Component {
+  static navigationOptions = {
+    title: 'List 列表',
+  }
+  render(){
+    return (
+       <ListExample />
+      );
+  }
+}
+class DialogExampleScreen extends React.Component {
+  static navigationOptions = {
+    title: 'Dialog 对话框',
+  }
+  render(){
+    return (
+       <DialogExample />
+      );
+  }
+}
+class InputExampleScreen extends React.Component {
+  static navigationOptions = {
+    title: 'Input 表单输入',
+  }
+  render(){
+    return (
+       <InputExample />
+      );
+  }
+}
+class ToastExampleScreen extends React.Component {
+  static navigationOptions = {
+    title: 'Toast弱提示',
+  }
+  render(){
+    return (
+       <ToastExample />
+      );
+  }
+}
+
+export default App = StackNavigator({
+  Home:{ screen: HomeScreen },
+  ButtonExample:{screen: ButtonExampleScreen },
+  ListExample:{screen: ListExampleScreen },
+  DialogExample:{screen: DialogExampleScreen },
+  InputExample:{screen: InputExampleScreen },
+  ToastExample:{screen: ToastExampleScreen },
+});
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection:'column',
+    backgroundColor: '#fff',
+    //alignItems: 'center',
+    justifyContent: 'flex-start',
+  },
+  ysuiBtn: {
+    backgroundColor: '#fd783f',
+    height:44,
+    paddingLeft: 20,
+    paddingRight: 20,
+    paddingTop: 0,
+    paddingBottom: 0,
+    width: 40,
+    borderRadius: 44,
+    borderColor:'#fd783f',
+    borderStyle:'solid',
+    borderWidth:1,
+    marginTop:20,
+  },
+  title: {
+    height:44,
+    padding:20,
+    fontSize:18,
+  }
+});
+
+
---
+++ example/List.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { StyleSheet, Text, View, TouchableHighlight} from 'react-native';
+// import { Toast } from '../src/index';
+
+export default class ListExample extends React.Component {
+  constructor(props) {
+        super(props);
+    }
+  componentDidMount(){
+    // this.refs.toast.show('hahha',2000);
+  }
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>规划中...</Text>
+      </View>
+    );
+  }
+}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  ysuiBtn: {
+    backgroundColor: '#fd783f',
+    height:44,
+    paddingLeft: 20,
+    paddingRight: 20,
+    paddingTop: 0,
+    paddingBottom: 0,
+    width: 'auto',
+    borderRadius: 44,
+    marginTop:20,
+    justifyContent: 'center',
+  },
+  text: {
+    color: 'white',
+  }
+});
